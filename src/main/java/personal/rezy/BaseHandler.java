68e7cdac8cd7e35a06376c736fe4c75b45f4cac8
rz-yang <yangrz@smail.nju.edu.cn>
2024-03-11 17:08:22 +0800

    define handler class

--- src/main/java/personal/rezy/BaseHandler.java
+++ src/main/java/personal/rezy/BaseHandler.java
@@ -1,4 +1,18 @@
 package personal.rezy;
 
-public class BaseHandler {
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+
+public abstract class BaseHandler<T> extends SimpleChannelInboundHandler<T> {
+
+    // 接收信息和ctx业务数据，并处理
+    @Override
+    protected void channelRead0(ChannelHandlerContext ctx, T msg) {
+        session(ctx, msg, ctx.channel());
+
+    }
+
+    // 处理的输出结果到ctx的channel里边
+    protected abstract void session(ChannelHandlerContext ctx, T requset, final Channel chan);
 }
---
+++ src/main/java/personal/rezy/handlers/SessionServerHandler.java
@@ -0,0 +1,43 @@
+package personal.rezy.handlers;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.serializer.SerializerFeature;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.handler.codec.http.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import personal.rezy.BaseHandler;
+
+
+// 具体的Handler实现类，进行处理
+public class SessionServerHandler extends SimpleChannelInboundHandler<FullHttpRequest> {
+    private final Logger logger = LoggerFactory.getLogger(SessionServerHandler.class);
+
+    @Override
+    protected void channelRead0(ChannelHandlerContext channelHandlerContext, FullHttpRequest fullHttpRequest) throws Exception {
+        logger.info("网关收到的请求为 url: {} method: {}", fullHttpRequest.uri(), fullHttpRequest.method());
+
+        // 返回的HTTP响应结构体
+        DefaultFullHttpResponse response = new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.OK);
+        // 响应结构体的content内容
+        response.content().writeBytes(JSON.toJSONBytes("我接受到你的请求了，然后我回你这个。另外你的url是" + fullHttpRequest.uri(), SerializerFeature.PrettyFormat));
+        // 返回的HTTP头信息
+        HttpHeaders headers = response.headers();
+        // 设置头信息：返回类别
+        headers.add(HttpHeaderNames.CONTENT_TYPE, HttpHeaderValues.APPLICATION_JSON + "; charset=UTF-8");
+        // 设置头信息：响应体长度
+        headers.add(HttpHeaderNames.CONTENT_LENGTH, response.content().readableBytes());
+        // 设置头信息：长连接
+        headers.add(HttpHeaderNames.CONNECTION, HttpHeaderValues.KEEP_ALIVE);
+        // 设置头信息：配置跨域访问
+        headers.add(HttpHeaderNames.ACCESS_CONTROL_ALLOW_ORIGIN, "*");
+        headers.add(HttpHeaderNames.ACCESS_CONTROL_ALLOW_HEADERS, "*");
+        headers.add(HttpHeaderNames.ACCESS_CONTROL_ALLOW_METHODS, "GET, POST, PUT, DELETE");
+        headers.add(HttpHeaderNames.ACCESS_CONTROL_ALLOW_CREDENTIALS, "true");
+
+        channelHandlerContext.channel().writeAndFlush(response);
+    }
+
+
+}
